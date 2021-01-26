d649ca925892963d64438152212c823f47640561
Teddy Aleksov <teddy57603@gmail.com>
2021-01-26 17:19:35 +0200

    added ex 02.SumNumbers

---
+++ 02.SumNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.SumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Func<string, int> parses = n => int.Parse(n);
+
+            int[] numbers = input
+                .Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(parses)
+                .ToArray();
+
+            Console.WriteLine(numbers.Length);
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}
