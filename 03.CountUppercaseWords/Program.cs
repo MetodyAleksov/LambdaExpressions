a1a41f79487eed9dc83e58860380cd6e6073041a
Teddy Aleksov <teddy57603@gmail.com>
2021-01-26 17:29:43 +0200

    added ex.03.PrintUpper

---
+++ 03.CountUppercaseWords/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.CountUppercaseWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, bool> checker = x => Char.IsUpper(x[0]);
+
+            string[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Where(checker)
+                .ToArray();
+
+            Console.WriteLine(string.Join(Environment.NewLine, input));
+        }
+    }
+}
